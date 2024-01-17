9420ef1615fac58baae7c439cfd0e6bfd65386c3
gamze <gamzekandara@gmail.com>
2024-01-17 18:10:54 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const employee = { name : "value1", streetAddress : "value2"};
+const employee = { name : "Gamze", streetAddress : "38th street"}; //returns an employee with the original key value pairs and the new key value pair'
+let newObj = {...employee};
 
-function updateEmployeeWithKeyAndValue(employee, key,value) {
-    const newObj = {...employee};
+function updateEmployeeWithKeyAndValue(employee, key , value){
+    
+//  update key:value
     newObj[key] = value;
-    return newObj; 
-}
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value; 
-    const newObj = {...employee};
     return newObj;
 }
+console.log(updateEmployeeWithKeyAndValue(employee, "name" , "Sam"));
+console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress" , "11  Broadway"));
 
-function deleteFromEmployeeByKey(employee, key) {
-    const newObj = {...employee};
-    delete newObj[key];
-    return newObj;
-}
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]; 
-    const newObj = {...employee};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
     return employee;
+};
 
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "Sam" , "12 Broadway"));
+
+
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {...employee} 
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+console.log(deleteFromEmployeeByKey(employee, 'name'));
+
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
 }
+
+let newEmpObj = destructivelyDeleteFromEmployeeByKey(employee, 'name')
